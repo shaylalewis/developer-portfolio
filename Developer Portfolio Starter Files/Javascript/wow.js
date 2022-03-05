e54a1255d59fe8c9f9960313950f8e728bf0ba3d
crime boy <62856686+shaylalewis@users.noreply.github.com>
2022-03-05 00:49:39 -0500

    added styling
    finished html and started css. added js as well for hamburger menu on mobile

---
+++ Developer Portfolio Starter Files/Javascript/wow.js
@@ -0,0 +1,9 @@
+function gfgMenu() {
+    const GFG = document.querySelector('.links');
+
+    if (GFG.style.display === "none") {
+        GFG.style.display = "block";
+    } else {
+        GFG.style.display = "none";
+    }
+}
